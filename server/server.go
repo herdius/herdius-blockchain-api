fb72a470d4cfcf8c7a5c6efcfef80c649eee46b5
Cole Bittel <cole.bittel@gmail.com>
2019-03-11 11:38:17 +0100

    Add getaccountportfolio

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -20,6 +21,7 @@ func main() {
 // CRUD requests are performed
 func LaunchServer() {
 	http.HandleFunc("/getaccount", GetAccount)
+	http.HandleFunc("/getaccountportfolio", GetAccountPortfolio)
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
@@ -27,16 +29,35 @@ func LaunchServer() {
 // tokens and returns an account Detail of the associated Herdius token address
 func GetAccount(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "\nRequest Pending\n")
+	acct, err := validateInput(w, r)
+	if err != nil {
+		fmt.Fprint(w, err)
+		return
+	}
+	log.Println("Account address:", acct.Address)
+}
+
+func GetAccountPortfolio(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "\nRequest Pending\n")
+	acct, err := validateInput(w, r)
+	if err != nil {
+		fmt.Fprint(w, err)
+		return
+	}
+	log.Println("Account address:", acct.Address)
+}
+
+func validateInput(w http.ResponseWriter, r *http.Request) (*account.Account, error) {
 	params, ok := r.URL.Query()["address"]
 	if !ok || len(params[0]) < 1 {
 		log.Println("Url Param 'address' is missing")
 		fmt.Fprint(w, "Request invalid, 'address' param missing\n")
-		return
+		return nil, errors.New("Invalid address parameter provided")
 	}
 	addressJson := params[0]
 	account := &account.Account{}
 	account.Address = addressJson
 
 	fmt.Fprint(w, "Request Valid\n")
-	log.Println("Account address:", account.Address)
+	return account, nil
 }
