9299adc43068e40b4488718d53e20ae5bff6bb87
Lingraj Mahanand <mlingraj@medipedia.io>
2019-04-09 23:48:46 +0530

    Removed TXMessagePlugin

--- handler/tx.go
+++ handler/tx.go
@@ -18,9 +18,6 @@ import (
 	"github.com/herdius/herdius-core/p2p/network"
 )
 
-// TXMessagePlugin will receive all Transaction specific messages.
-type TXMessagePlugin struct{ *network.Plugin }
-
 func (s *service) SendTxToBlockchain(txReq protobuf.TxRequest) (*protobuf.TxResponse, error) {
 	net, err := nb.GetNetworkBuilder().Build()
 	if err != nil {
