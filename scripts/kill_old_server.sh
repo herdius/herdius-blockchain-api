c428ae04577321757806c02bac54777da4d530d4
Cole Bittel <cole.bittel@gmail.com>
2019-04-11 11:09:41 +0200

    remove old pid file

--- scripts/kill_old_server.sh
+++ scripts/kill_old_server.sh
@@ -1,20 +1,5 @@
-set -ex
+set -x
 
 echo "attempting to kill old server proc"
-
-whoami
-pwd
-ls -lah
-cd /var/run
-ls -lah
-ls -lah | grep api-server.pid
-! pkill -f /tmp/go-build
-
-if [ $? -gt 0 ]
-then
-	echo "no pid found for api server, api server not running"
-else
-	echo "api server running, killing server"
-	kill `cat api-server.pid`
-	echo "api server destroyed"
-fi
+pkill -f /tmp/go-build
+echo "old server killed"
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,13 +1,11 @@
 set -ex
 
-echo "this yaaa is a working cicd pipeline test"
 whoami
 pwd
 ls -lah
 source /etc/bashrc
 echo "GOPATH = $GOPATH"
 cd /root/go/src/github.com/herdius/herdius-blockchain-api
-ls -lah
 go get ./...
 go run /root/go/src/github.com/herdius/herdius-blockchain-api/server/server.go &
-echo $! > /var/run/api-server.pid
+echo "server started in background"
