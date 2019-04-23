7cba20272b7082193f43a5814c7ae95d592a43b1
Cole Bittel <cole.bittel@gmail.com>
2019-04-23 17:17:44 +0200

    Need a gopath to deploy

--- deployment/start_server.sh
+++ deployment/start_server.sh
@@ -3,6 +3,7 @@ set -ex
 
 source /etc/bashrc
 export ENV=staging
+export GOPATH=/root/go
 cd /root/go/src/github.com/herdius/herdius-blockchain-api
 go get ./...
 go run /root/go/src/github.com/herdius/herdius-blockchain-api/server/server.go -env=staging > /dev/null 2> /dev/null < /dev/null &
