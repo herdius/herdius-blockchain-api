4806932366fd6551cca6a11d4268451c2aba3daf
Cole Bittel <cole.bittel@gmail.com>
2019-05-06 12:30:54 +0200

    client.go send multiple txs; logging to file not stdout

--- deployment/start_server.sh
+++ deployment/start_server.sh
@@ -5,6 +5,7 @@ source /etc/bashrc
 export ENV=staging
 export GOPATH=/root/go
 cd /root/go/src/github.com/herdius/herdius-blockchain-api
+mkdir -p /var/log/herdius/herdius-blockchain-api/log/
 go get ./...
-go run /root/go/src/github.com/herdius/herdius-blockchain-api/server/server.go -env=staging > /dev/null 2> /dev/null < /dev/null &
+go run /root/go/src/github.com/herdius/herdius-blockchain-api/server/server.go -env=staging > /var/log/herdius/herdius-blockchain-api/log/server.log 2> /var/log/herdius/herdius-blockchain-api/log/server.log < /dev/null &
 echo "server started in background"
