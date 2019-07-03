86f70d1006857e2a402d1c028a19a980db908755
Lingraj Mahanand <mlingraj@medipedia.io>
2019-07-03 14:03:21 +0530

    modified the client.go file to process all type of transactions at once We need this to make sure that existing functionalities are not breaking due to new changes

--- client.go
+++ client.go
@@ -29,44 +29,212 @@ func main() {
 	var endpoint string
 	endpoint = *host
 
-	endpoint = "http://" + endpoint + ":80/tx"
+	endpoint = "http://" + endpoint + ":80"
 	log.Println("endpoint:", endpoint)
-
-	if strings.EqualFold(*txType, "update") {
+	switch *txType {
+	case "all":
+		performAllTxs(endpoint)
+	case "register":
 		sendAccountRegisterTx(endpoint)
-	} else if strings.EqualFold(*txType, "external") {
+	case "external":
 		postExternalTx(endpoint)
-	} else {
+	case "internal":
 		postTx(endpoint)
 	}
 
 }
 
-func sendAccountRegisterTx(endpoint string) {
-	// Create key pairs and store in a local file
-	// User 1
-	// Address: HHy1CuT3UxCGJ3BHydLEvR5ut6HRy2qUvm
+// This function always registers a new HER Account.
+// It randomly creates a private key and uses the same key to register HER account
+// Once the new HER Account is added, it will add a ETH address to new HER Account
+func performAllTxs(endpoint string) {
 
-	// ETH: 0xD8f647855876549d2623f52126CE40D053a2ef6A
-	senderPrivKey, err := key.LoadOrGenNodeKey("./tempKey.json")
-
-	// User 2
-	// Address: HKTXmdsHyZn1B2ErRKiG4iN34YixCgdQgx
-	//Eth Add: 0x9aA7E9819D781eFf5B239b572c4Fe8F964a899c9
-	senderPrivKey, err = key.LoadOrGenNodeKey("./tempKeySign.json")
-
-	if err != nil {
-		panic(err)
-	}
-
-	sendPubKey := senderPrivKey.PubKey()
+	senderPrivKey := secp256k1.GenPrivKey()
+	senderAddress := senderPrivKey.PubKey().GetAddress()
 	var pubkeyBytes secp256k1.PubKeySecp256k1
+	sendPubKey := senderPrivKey.PubKey()
 	pubkeyBytes = sendPubKey.(secp256k1.PubKeySecp256k1)
 
 	senderB64 := b64.StdEncoding.EncodeToString(pubkeyBytes[:])
-	senderAddress := sendPubKey.GetAddress()
-	log.Println("Account update request : " + senderAddress)
-	msg := "Update my account"
+	log.Println("Account Address : " + senderAddress)
+
+	msg := "Register New HER Account"
+
+	asset := &protobuf.Asset{
+		Category:              "crypto",
+		Symbol:                "HER",
+		Network:               "Herdius",
+		Value:                 15,
+		Fee:                   0,
+		Nonce:                 0,
+		ExternalSenderAddress: "",
+	}
+	tx := protobuf.Tx{
+		SenderAddress: senderAddress,
+		SenderPubkey:  senderB64,
+		Message:       msg,
+		Type:          "update",
+		Asset:         asset,
+	}
+
+	// Sign the transaction detail
+	txbBeforeSign, _ := json.Marshal(tx)
+	fmt.Println("txbBeforeSign: ", string(txbBeforeSign))
+	signature, err := senderPrivKey.Sign(txbBeforeSign)
+	if err != nil {
+		log.Panic("Failed to sign: ", err)
+	}
+
+	tx.Sign = b64.StdEncoding.EncodeToString(signature)
+
+	// Post tx to blockchain.
+	txReq := protobuf.TxRequest{
+		Tx: &tx,
+	}
+	txJSON, err := json.Marshal(txReq)
+	if err != nil {
+		log.Fatalf("Failed to Marshal %v", err)
+	}
+	response, err := http.Post(endpoint+"/tx", "application/json", bytes.NewBuffer(txJSON))
+	if err != nil {
+		log.Fatalf("Failed to Marshal %v", err)
+	}
+
+	defer response.Body.Close()
+	body, readErr := ioutil.ReadAll(response.Body)
+	if readErr != nil {
+		log.Fatalf("Failed to read http response %s.", readErr)
+	}
+	var txResponse protobuf.TxResponse
+	jsonErr := json.Unmarshal(body, &txResponse)
+	if jsonErr != nil {
+		log.Fatalf("Failed to Unmarshal %s.", jsonErr)
+	}
+
+	log.Println(txResponse.TxId)
+	log.Println(txResponse.Status)
+
+	log.Println("Retrieving new HER Account detail : ", senderAddress)
+	getURL := endpoint + "/account/" + senderAddress
+	log.Println("Get account url: ", getURL)
+	for {
+		response, err = http.Get(getURL)
+		if err != nil {
+			log.Println("Failed to get response: ", err)
+		} else {
+			defer response.Body.Close()
+			body, readErr := ioutil.ReadAll(response.Body)
+			if readErr != nil {
+				log.Println(fmt.Sprintf("Failed to read http response %s.", readErr))
+			}
+			if strings.Contains(string(body), "details not found") {
+				continue
+			}
+			log.Println("Account response: ", string(body))
+			break
+		}
+	}
+
+	log.Println("Register an ethereum address to created HER account: ", senderAddress)
+
+	asset = &protobuf.Asset{
+		Category:              "crypto",
+		Symbol:                "ETH",
+		Network:               "Herdius",
+		Value:                 0,
+		Fee:                   0,
+		Nonce:                 1,
+		ExternalSenderAddress: "0xD8f647855876549d2623f52126CE40D053a2ef6A",
+	}
+
+	tx = protobuf.Tx{
+		SenderAddress: senderAddress,
+		SenderPubkey:  senderB64,
+		Message:       msg,
+		Type:          "update",
+		Asset:         asset,
+	}
+
+	// Sign the transaction detail
+	txbBeforeSign, _ = json.Marshal(tx)
+	fmt.Println("txbBeforeSign: ", string(txbBeforeSign))
+	signature, err = senderPrivKey.Sign(txbBeforeSign)
+	if err != nil {
+		log.Panic("Failed to sign: ", err)
+	}
+
+	tx.Sign = b64.StdEncoding.EncodeToString(signature)
+
+	// Post tx to blockchain.
+	txReq = protobuf.TxRequest{
+		Tx: &tx,
+	}
+	txJSON, err = json.Marshal(txReq)
+	if err != nil {
+		log.Fatalf("Failed to Marshal %v", err)
+	}
+	response, err = http.Post(endpoint+"/tx", "application/json", bytes.NewBuffer(txJSON))
+	if err != nil {
+		log.Fatalf("Failed to Marshal %v", err)
+	}
+
+	defer response.Body.Close()
+
+	if readErr != nil {
+		log.Fatalf("Failed to read http response %s.", readErr)
+	}
+
+	jsonErr = json.Unmarshal(body, &txResponse)
+	if jsonErr != nil {
+		log.Fatalf("Failed to Unmarshal %s.", jsonErr)
+	}
+
+	log.Println(txResponse.TxId)
+	log.Println(txResponse.Status)
+
+	log.Println("Retrieving new HER Account detail : ", senderAddress)
+	getURL = endpoint + "/account/" + senderAddress
+	log.Println("Get account url: ", getURL)
+	for {
+		response, err = http.Get(getURL)
+		if err != nil {
+			log.Println("Failed to get response: ", err)
+		} else {
+			defer response.Body.Close()
+			body, readErr := ioutil.ReadAll(response.Body)
+			if readErr != nil {
+				log.Println(fmt.Sprintf("Failed to read http response %s.", readErr))
+			}
+			if strings.Contains(string(body), "details not found") {
+				continue
+			}
+			var accRes protobuf.AccountResponse
+			jsonErr := json.Unmarshal(body, &accRes)
+			if jsonErr != nil {
+				log.Fatalf("Failed to Unmarshal %s.", jsonErr)
+			}
+			if len(accRes.EBalances) == 0 {
+				continue
+			}
+			log.Println("Account response: ", accRes)
+			break
+		}
+	}
+}
+
+func sendAccountRegisterTx(endpoint string) {
+	senderPrivKey := secp256k1.GenPrivKey()
+	senderAddress := senderPrivKey.PubKey().GetAddress()
+	var pubkeyBytes secp256k1.PubKeySecp256k1
+	sendPubKey := senderPrivKey.PubKey()
+	pubkeyBytes = sendPubKey.(secp256k1.PubKeySecp256k1)
+
+	senderB64 := b64.StdEncoding.EncodeToString(pubkeyBytes[:])
+	log.Println("Account Address : " + senderAddress)
+
+	pubkeyBytes = sendPubKey.(secp256k1.PubKeySecp256k1)
+
+	msg := "Register HER account"
 	asset := &protobuf.Asset{
 		Category:              "crypto",
 		Symbol:                "HER",
@@ -101,7 +269,7 @@ func sendAccountRegisterTx(endpoint string) {
 	// Sign the transaction detail
 	txbBeforeSign, _ := json.Marshal(tx)
 
-	sig, err := senderPrivKey.PrivKey.Sign(txbBeforeSign)
+	sig, err := senderPrivKey.Sign(txbBeforeSign)
 
 	tx.Sign = b64.StdEncoding.EncodeToString(sig)
 	// Post tx to blockchain.
