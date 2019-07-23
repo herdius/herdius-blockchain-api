b6101fd306d8d504eca84243ab3f8c4673daffbd
LE Manh Cuong <cuong.manhle.vn@gmail.com>
2019-07-23 15:03:43 +0700

    store: query until latest block

--- store/sync.go
+++ store/sync.go
@@ -3,6 +3,7 @@ package store
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/herdius/herdius-blockchain-api/config"
 	"github.com/herdius/herdius-blockchain-api/protobuf"
@@ -19,21 +20,40 @@ func SyncPendingTxs(s Storer, net *network.Network, env string) error {
 		return fmt.Errorf("s.GetLatestBlockID: %v", err)
 	}
 
-	ctx := network.WithSignMessage(context.Background(), true)
-	req := protobuf.TxsByBlockHeightRequest{BlockHeight: int64(blockID) + 1}
-	res, err := supervisorNode.Request(ctx, &req)
-	if err != nil {
-		return fmt.Errorf("supervisorNode.Request: %v", err)
-	}
+	for {
+		blockID++
+		ctx := network.WithSignMessage(context.Background(), true)
+		req := protobuf.BlockHeightRequest{BlockHeight: blockID}
+		msg, err := supervisorNode.Request(ctx, &req)
+		if err != nil {
+			log.Printf("failed to query block info: %d\n", blockID)
+			break
+		}
 
-	if msg, ok := res.(*protobuf.TxsResponse); ok {
-		for _, tx := range msg.GetTxs() {
-			if tx.Tx == nil {
-				continue
+		block, ok := msg.(*protobuf.BlockResponse)
+		if !ok || block.Time == nil {
+			log.Printf("block %d does not exist\n", blockID)
+			break
+		}
+
+		ctx = network.WithSignMessage(context.Background(), true)
+		txsReq := protobuf.TxsByBlockHeightRequest{BlockHeight: blockID}
+		res, err := supervisorNode.Request(ctx, &txsReq)
+		if err != nil {
+			log.Printf("failed to get txs in block: %d\n", blockID)
+			return fmt.Errorf("supervisorNode.Request: %v", err)
+		}
+
+		if msg, ok := res.(*protobuf.TxsResponse); ok {
+			for _, tx := range msg.GetTxs() {
+				if tx.Tx == nil {
+					continue
+				}
+				if err := s.Update(FromTxDetailResponse(tx)); err != nil {
+					return err
+				}
 			}
-			if err := s.Update(FromTxDetailResponse(tx)); err != nil {
-				return err
-			}
+			return nil
 		}
 	}
 	return nil
