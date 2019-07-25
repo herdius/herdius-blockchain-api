6b4b7f8cc41f53d5127947e7eb5a7f8c605d409e
LE Manh Cuong <cuong.manhle.vn@gmail.com>
2019-07-25 10:10:08 +0700

    deployment: use supervisor to manage service

--- deployment/start_server.sh
+++ deployment/start_server.sh
@@ -1,31 +1,16 @@
 #!/usr/bin/env bash
+
 set -ex
 
 source /etc/bashrc
 export ENV=staging
 export GOPATH=/root/go
+service_name="herdius-api"
 cd /root/go/src/github.com/herdius/herdius-blockchain-api
-mkdir -p /var/log/herdius/herdius-blockchain-api/log/
-RUNDIR=/var/run/herdius
-if [ ! -d "$RUNDIR" ]; then
-  mkdir -p "$RUNDIR"
-fi
 
-pidfile="$RUNDIR/api-server.pid"
-
-# Kill old process if existed
-if [[ -f "$pidfile" ]]; then
-  kill "$(cat "$pidfile")" || :
-fi
-
-# Wait api server quit
-sleep 5
+supervisorctl stop "$service_name"
 
 export GO111MODULE=on
 go build -o ./api-server ./server/server.go
-./api-server -env=staging > /var/log/herdius/herdius-blockchain-api/log/server.log 2>&1 </dev/null &
 
-# Save the pid to kill later
-printf '%s\n' "$!" >"$pidfile"
-
-echo "server started in background"
+supervisorctl start "$service_name"
