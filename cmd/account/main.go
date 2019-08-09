8c251d6aa518f3b7e39962d146fecfdd0fd4c082
Lingraj Mahanand <mlingraj@medipedia.io>
2019-08-09 08:50:03 +0530

    hexified private key

--- cmd/account/main.go
+++ cmd/account/main.go
@@ -1,73 +1,33 @@
 package main
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
 	"flag"
 	"fmt"
-	"log"
-	"math/big"
 
 	"github.com/btcd/btcec"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/herdius/herdius-core/crypto/secp256k1"
 )
 
 /*
-	To run it execute the below command
-	go run cmd/account/main.go -secret=My-Secret
+	To get herdius address against a hexified private key, execute the below command:
+	go run cmd/account/main.go -hexprivkey=00000000000000000000000000000000000000000000004d792d536563726575
 */
 func main() {
-	secret := flag.String("secret", "default", "secret to create private key")
+	hexPrivateKey := flag.String("hexprivkey", "default", "Hexified Private Key")
 	flag.Parse()
-	fmt.Println(GetHerdiusAddress(*secret))
+	fmt.Println(GetHerdiusAddress(*hexPrivateKey))
 }
 
 // GetHerdiusAddress creates a Herdius Address based
-// on the provided secret to private key
-
-func GetHerdiusAddress(secret string) string {
-	privateKey := createPrivateKey([]byte(secret))
+// on the provided hexPrivateKey to private key
+func GetHerdiusAddress(hexPrivateKey string) string {
 	var senderPrivKey btcec.PrivateKey
-	senderPrivKey = btcec.PrivateKey(*privateKey)
+	ecdsaPK, _ := crypto.HexToECDSA(hexPrivateKey)
+	senderPrivKey = btcec.PrivateKey(*ecdsaPK)
 
 	var sendPubKey secp256k1.PubKeySecp256k1
 	copy(sendPubKey[:], senderPrivKey.PubKey().SerializeCompressed())
 	senderAddress := sendPubKey.GetAddress()
 	return senderAddress
-}
-
-// GetPrivateKey ...
-func createPrivateKey(ch []byte) *ecdsa.PrivateKey {
-	bigInt, err := createBigInt(btcec.S256(), ch)
-	if err != nil {
-		log.Printf("Failed to create big number : %v", err)
-		return nil
-	}
-
-	//Create Private Key using Big Int
-	privKey, _ := generateKey(btcec.S256(), bigInt)
-	return privKey
-}
-
-// generateKey generates a public and private key pair.
-func generateKey(c elliptic.Curve, k *big.Int) (*ecdsa.PrivateKey, error) {
-
-	priv := new(ecdsa.PrivateKey)
-	priv.PublicKey.Curve = c
-	priv.D = k
-	priv.PublicKey.X, priv.PublicKey.Y = c.ScalarBaseMult(k.Bytes())
-	return priv, nil
-}
-
-// Create Big Int underlying the given
-// curve using the procedure given in [NSA] A.2.1.
-func createBigInt(c elliptic.Curve, ch []byte) (k *big.Int, err error) {
-	params := c.Params()
-	one := new(big.Int).SetInt64(1)
-
-	k = new(big.Int).SetBytes(ch)
-	n := new(big.Int).Sub(params.N, one)
-	k.Mod(k, n)
-	k.Add(k, one)
-	return
 }
